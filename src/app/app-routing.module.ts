eee8936f0c5d44e5178c9085441e1afa70a915ae
Marc <1440411korver@zuyd.nl>
2019-03-01 17:45:40 +0100

    added pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,12 +9,44 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: './home/home.module#HomePageModule'
+    loadChildren: './pages/home/home.module#HomePageModule'
   },
   {
     path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
-  }
+    loadChildren: './pages/list/list.module#ListPageModule'
+  },
+  { path: 'action-sheet', loadChildren: './pages/action-sheet/action-sheet.module#ActionSheetPageModule' },
+  { path: 'alert', loadChildren: './pages/alert/alert.module#AlertPageModule' },
+  { path: 'badge', loadChildren: './pages/badge/badge.module#BadgePageModule' },
+  { path: 'button', loadChildren: './pages/button/button.module#ButtonPageModule' },
+  { path: 'card', loadChildren: './pages/card/card.module#CardPageModule' },
+  { path: 'checkbox', loadChildren: './pages/checkbox/checkbox.module#CheckboxPageModule' },
+  { path: 'chip', loadChildren: './pages/chip/chip.module#ChipPageModule' },
+  { path: 'content', loadChildren: './pages/content/content.module#ContentPageModule' },
+  { path: 'date-time', loadChildren: './pages/date-time/date-time.module#DateTimePageModule' },
+  { path: 'floating-action-button', loadChildren: './pages/floating-action-button/floating-action-button.module#FloatingActionButtonPageModule' },
+  { path: 'grid', loadChildren: './pages/grid/grid.module#GridPageModule' },
+  { path: 'infinite-scroll', loadChildren: './pages/infinite-scroll/infinite-scroll.module#InfiniteScrollPageModule' },
+  { path: 'icons', loadChildren: './pages/icons/icons.module#IconsPageModule' },
+  { path: 'input', loadChildren: './pages/input/input.module#InputPageModule' },
+  { path: 'item', loadChildren: './pages/item/item.module#ItemPageModule' },
+  { path: 'media', loadChildren: './pages/media/media.module#MediaPageModule' },
+  { path: 'modal', loadChildren: './pages/modal/modal.module#ModalPageModule' },
+  { path: 'navigation', loadChildren: './pages/navigation/navigation.module#NavigationPageModule' },
+  { path: 'popover', loadChildren: './pages/popover/popover.module#PopoverPageModule' },
+  { path: 'progress-indicators', loadChildren: './pages/progress-indicators/progress-indicators.module#ProgressIndicatorsPageModule' },
+  { path: 'radio', loadChildren: './pages/radio/radio.module#RadioPageModule' },
+  { path: 'range', loadChildren: './pages/range/range.module#RangePageModule' },
+  { path: 'refresher', loadChildren: './pages/refresher/refresher.module#RefresherPageModule' },
+  { path: 'reorder', loadChildren: './pages/reorder/reorder.module#ReorderPageModule' },
+  { path: 'searchbar', loadChildren: './pages/searchbar/searchbar.module#SearchbarPageModule' },
+  { path: 'segment', loadChildren: './pages/segment/segment.module#SegmentPageModule' },
+  { path: 'select', loadChildren: './pages/select/select.module#SelectPageModule' },
+  { path: 'slides', loadChildren: './pages/slides/slides.module#SlidesPageModule' },
+  { path: 'tabs', loadChildren: './pages/tabs/tabs.module#TabsPageModule' },
+  { path: 'toast', loadChildren: './pages/toast/toast.module#ToastPageModule' },
+  { path: 'toggle', loadChildren: './pages/toggle/toggle.module#TogglePageModule' },
+  { path: 'typography', loadChildren: './pages/typography/typography.module#TypographyPageModule' }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,119 @@ export class AppComponent {
   public appPages = [
     {
       title: 'Home',
-      url: '/home',
+      url: '/home'
+    },
+    {
+      title: 'Action Sheet',
+      url: '/action-sheet'
+    },
+    {
+      title: 'Alert',
+      url: '/alert'
+    },
+    {
+      title: 'Badge',
+      url: '/badge'
+    },
+    {
+      title: 'Button',
+      url: '/button'
+    },
+    {
+      title: 'Card',
+      url: '/card'
+    },
+    {
+      title: 'Checkbox',
+      url: '/checkbox'
+    },
+    {
+      title: 'Chip',
+      url: '/chip'
+    },
+    {
+      title: 'Content',
+      url: '/content'
+    },
+    {
+      title: 'Date & Time pickers',
+      url: '/date-time'
+    },
+    {
+      title: 'Floating Action Button',
+      url: '/floating-action-button'
+    },
+    {
+      title: 'Grid',
+      url: '/grid'
+    },
+    {
+      title: 'Media',
+      url: '/media'
+    },
+    {
+      title: 'Modal',
+      url: '/modal'
+    },
+    {
+      title: 'Navigation',
+      url: '/navigation'
+    },
+    {
+      title: 'Popover',
+      url: '/popover'
+    },
+    {
+      title: 'Progress indicators',
+      url: '/progress-indicators'
+    },
+    {
+      title: 'Radio',
+      url: '/radio'
+    },
+    {
+      title: 'Range',
+      url: '/range'
+    },
+    {
+      title: 'Refresher',
+      url: '/refresher'
+    },
+    {
+      title: 'Reorder',
+      url: '/reorder',
       icon: 'home'
     },
     {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
+      title: 'Searchbar',
+      url: '/searchbar'
+    },
+    {
+      title: 'Segment',
+      url: '/segment'
+    },
+    {
+      title: 'Select',
+      url: '/select'
+    },
+    {
+      title: 'Slides'
+    },
+    {
+      title: 'Tabs',
+      url: '/tabs'
+    },
+    {
+      title: 'Toast',
+      url: '/toast'
+    },
+    {
+      title: 'Toggle',
+      url: '/toggle'
+    },
+    {
+      title: 'Typography',
+      url: '/typography'
     }
   ];
 
